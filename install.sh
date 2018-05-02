a1173c315fc5035e9ab947b03cf64a7f66e9c3cd
masumi <taketo224@gmail.com>
2018-05-02 11:17:21 +0900

    va

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if ! grep XDG_CONFIG_HOME $HOME/.bashrc > /dev/null; then
+	echo "export XDG_CONFIG_HOME=$HOME/.config" >> $HOME/.bashrc
+	. $HOME/.bashrc
+fi
 
 if grep Comp $XDG_CONFIG_HOME/nvim/init.vim > /dev/null; then
 	echo "CompProMode has been installed."
