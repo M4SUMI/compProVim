633764f3a52f65f015a995529657d882d10cf55b
masumi <taketo224@gmail.com>
2018-07-20 22:14:37 +0900

    補完のプラグインを変えたことによる修正

--- install.sh
+++ install.sh
@@ -2,13 +2,25 @@
 if ! grep XDG_CONFIG_HOME $HOME/.bashrc > /dev/null; then
 	echo "export XDG_CONFIG_HOME=$HOME/.config" >> $HOME/.bashrc
 	. $HOME/.bashrc
+    return
 fi
 
 if grep Comp $XDG_CONFIG_HOME/nvim/init.vim > /dev/null; then
 	echo "CompProMode has been installed."
 else
-    echo "\"CompProMode"                              >> $XDG_CONFIG_HOME/nvim/init.vim
-    echo "if isdirectory(expand('$HOME/compProVim'))" >> $XDG_CONFIG_HOME/nvim/init.vim
-    echo "\tcommand! Comp source ${PWD}/execute.vim"  >> $XDG_CONFIG_HOME/nvim/init.vim
-    echo "endif"                                      >> $XDG_CONFIG_HOME/nvim/init.vim
+    echo "\"CompProMode"                               >> $XDG_CONFIG_HOME/nvim/init.vim
+    echo "if isdirectory(expand('$HOME/compProVim'))"  >> $XDG_CONFIG_HOME/nvim/init.vim
+    echo "    command! Comp source ${PWD}/execute.vim" >> $XDG_CONFIG_HOME/nvim/init.vim
+    echo "endif"                                       >> $XDG_CONFIG_HOME/nvim/init.vim
+
+    touch compile_commands.json
+    echo "["                                              >> compile_commands.json
+    echo "    {"                                          >> compile_commands.json
+    echo "        'command' : 'g++ -std=c++14 main.cpp'," >> compile_commands.json
+    echo "        'directory' : '${PWD}',"                >> compile_commands.json
+    echo "        'file' : 'main.cpp',"                   >> compile_commands.json
+    echo "    {"                                          >> compile_commands.json
+    echo "["                                              >> compile_commands.json
 fi
+
+
